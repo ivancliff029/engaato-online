4996a802c0a3c98e85650409d9d1269a0540b04e
ivancliff029 <odekeivancliff@gmail.com>
2024-09-21 15:33:46 +0300

    Update : Simple UI Fix

--- app/components/Welcome.tsx
+++ app/components/Welcome.tsx
@@ -7,7 +7,7 @@ const Welcome: React.FC = () => {
       <div className="flex flex-col items-center justify-center h-full text-white">
         <h1 className="text-5xl font-bold mb-4 p-4">Engaato Online</h1>
         <p className="text-lg mb-8 p-4">Step into Style, Comfort, and Confidence.</p>
-        <Link href="/store" legacyBehavior>
+        <Link href="/product" legacyBehavior>
           <a className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg flex items-center">
             Shop Now
             <svg
